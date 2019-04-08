13d958f0c0cc7462e78045174e1f74ec40b0061a
Todd Johnson <toddjohnson407@gmail.com>
2019-04-08 01:15:33 -0600

    minor margin adjustments

--- index.js
+++ index.js
@@ -7,25 +7,4 @@ app.get('/', (req, res) => res.sendFile('home.html', { root: __dirname + "/app"}
 
 module.exports = app;
 
-// var express = require('express');
-// var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
-
-// var indexRouter = require('./routes/index');
-// var usersRouter = require('./routes/users');
-
-// var app = express();
-
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// module.exports = app;
-
 app.listen(PORT, () => console.log('Application is running on port: ' + PORT));
