e5729d18faa58d50f90150ba156e7914497cc791
Todd Johnson <toddjohnson407@gmail.com>
2019-04-08 00:52:41 -0600

    'www' script adjustments

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+// const express = require('express');
+// const app = express();
+// const port = 3000;
 
-app.use(express.static('app'));
-app.get('/', (req, res) => res.sendFile('home.html', { root: __dirname + "/app"}))
-app.listen(port, () => console.log(`News Analysis active on port ${ port }`));
+// app.use(express.static('app'));
+// app.get('/', (req, res) => res.sendFile('home.html', { root: __dirname + "/app"}))
+// app.listen(port, () => console.log(`News Analysis active on port ${ port }`));
+
+// module.exports = app;
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// var indexRouter = require('./routes/index');
+// var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+module.exports = app;
